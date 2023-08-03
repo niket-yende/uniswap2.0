09a9432a5372c3dacf827353209af40f1a767ae2
Niket Yende <niket6@gmail.com>
2023-08-03 19:56:01 +0530

    Added react components & hooks

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0xFe35a4BF69b33B4bA1D1be854CB31c2F3918c4d7"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/XeHkNfMVT5q8onkhqHPu8y8ZELqQAYYE",
   },
 };
