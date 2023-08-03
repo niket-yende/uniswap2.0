e23dad41b124a29c0a112dd139a4dabf5903e40f
Niket Yende <niket6@gmail.com>
2023-08-03 21:19:49 +0530

    Added .env config params

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,6 +1,7 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "0xFe35a4BF69b33B4bA1D1be854CB31c2F3918c4d7"; 
+export const ROUTER_ADDRESS = "0x7E2cE1215d8336D9d3526222b71AE8acFA1952b4";
+// console.log('ROUTER_ADDRESS: ', ROUTER_ADDRESS); 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
