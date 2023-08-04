55f21ad7d1d1307f82bb58ee7f64d37bd2a022a6
Niket Yende <niket6@gmail.com>
2023-08-04 10:28:23 +0530

    Added webpack.config to set env variables

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,11 +1,12 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "0x7E2cE1215d8336D9d3526222b71AE8acFA1952b4";
-// console.log('ROUTER_ADDRESS: ', ROUTER_ADDRESS); 
+export const ROUTER_ADDRESS = process.env.REACT_APP_ROUTER_ADDRESS;
+console.log('ROUTER_ADDRESS: ', process.env.REACT_APP_ROUTER_ADDRESS);
+console.log('test: ', process.env.REACT_APP_ALCHEMY_GOERLI_ENDPOINT); 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/XeHkNfMVT5q8onkhqHPu8y8ZELqQAYYE",
+    [Goerli.chainId]: process.env.REACT_APP_ALCHEMY_GOERLI_ENDPOINT,
   },
 };
